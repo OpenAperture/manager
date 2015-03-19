3965cdfa19316c8145f13ec1cda9294e91e88b26
jordan0day <jordan.day@perceptivesoftware.com>
2015-03-19 12:48:45 -0500

    update etcd_cluster to use new fleet api

--- lib/project_omelette_manager/etcd_cluster/units.ex
+++ lib/project_omelette_manager/etcd_cluster/units.ex
@@ -1,23 +1,26 @@
 defmodule ProjectOmeletteManager.EtcdCluster.Units do
   require Logger
-  @type unit :: Map.t
   @type etcd_token :: String.t
+
+  @doc """
+  A pid representing an agent holding a ProjectOmeletteManager.SystemdUnit.
+  """
+  @type systemd_unit_pid :: pid
+
+  alias FleetApi.Etcd, as: Fleet
+  alias ProjectOmeletteManager.SystemdUnit
 
   @doc """
   Deploys new units to the cluster
   """ 
-  @spec deploy_units([unit], String.t, [integer]) :: [unit]
+  @spec deploy_units([FleetApi.Unit.t], String.t, [integer]) :: [systemd_unit_pid]
   def deploy_units(units, etcd_token, available_ports \\ []) do
-    ## TODO: Fix once FleetApi is in place
-    # existing_units = FleetApi.Unit.list!(etcd_token)
-    existing_units = []
+    {:ok, api_pid} = Fleet.start_link(etcd_token)
+    {:ok, existing_units} = Fleet.list_units(api_pid)
 
     num_instances = if available_ports == nil || available_ports == [] do
-      ## TODO: Fix once FleetApi is in place
-      # etcd_token
-      # |> FleetApi.Machine.list!
-      # |> length
-      0
+      {:ok, hosts} = Fleet.list_machines(api_pid)
+      length(hosts)
     else
       length(available_ports)
     end
@@ -26,48 +29,39 @@ defmodule ProjectOmeletteManager.EtcdCluster.Units do
   end
 
   # Executes a rolling cycle units on the cluster.
-  @spec cycle_units([unit], integer, String.t, [integer], [unit], [unit]) :: [unit]
-  defp cycle_units([unit | remaining_units], max_instances, etcd_token, available_ports, all_existing_units, newly_deployed_units) do
-    if unit == nil || unit["name"] == nil do
+  @spec cycle_units([FleetApi.Unit.t], integer, String.t, [integer], [FleetApi.Unit.t], [pid]) :: [FleetApi.Unit.t]
+  defp cycle_units([unit | remaining_units], max_instances, etcd_token, available_ports, all_existing_units, newly_deployed_unit_pids) do
+    if unit == nil || unit.name == nil do
       remaining_ports = available_ports
     else
-      [unit_name|_] = String.split(unit["name"], ".service")
+      [unit_name|_] = String.split(unit.name, ".service")
 
-      # List comprehension to pick collect any existing units with this unit name.
-      existing_units = for existing <- all_existing_units, String.contains?(existing["name"], unit_name), do: existing
+      # List comprehension to collect any existing units with this unit name.
+      existing_units = for existing <- all_existing_units, String.contains?(existing.name, unit_name), do: existing
 
       # if there are any instances left over (originally there were 4, now there are 3), tear them down
-      {remaining_units, newly_deployed_units, remaining_ports} = cycle_single_unit(unit, 0, max_instances, available_ports, etcd_token, {existing_units, [], []})
+      {remaining_units, newly_deployed_unit_pids, remaining_ports} = cycle_single_unit(unit, 0, max_instances, available_ports, etcd_token, {existing_units, [], []})
+
       teardown_units(remaining_units, etcd_token)
     end
 
-    cycle_units(remaining_units, max_instances, etcd_token, remaining_ports, all_existing_units, newly_deployed_units)
+    cycle_units(remaining_units, max_instances, etcd_token, remaining_ports, all_existing_units, newly_deployed_unit_pids)
   end
 
   # This function clause is the base state for the recursion used in the main `cycle_units` implementation.
-  defp cycle_units([], _, _, _, _, newly_deployed_units), do: newly_deployed_units
+  defp cycle_units([], _, _, _, _, newly_deployed_unit_pids), do: newly_deployed_unit_pids
 
-  defp cycle_single_unit(unit, instance_id, max_instances, available_ports, etcd_token, {existing_units, newly_deployed_units, remaining_ports}) do
+  @spec cycle_single_unit(FleetApi.Unit, integer, integer, [integer], String.t, {[FleetApi.Unit.t], [pid], [integer]}) :: :ok
+  defp cycle_single_unit(unit, instance_id, max_instances, available_ports, etcd_token, {existing_units, newly_deployed_unit_pids, remaining_ports}) do
     if (instance_id >= max_instances) do
       # We've maxed out our unit count, stop and return any existing units that need to be terminated.
-      {existing_units, newly_deployed_units, remaining_ports}
+      {existing_units, newly_deployed_unit_pids, remaining_ports}
     else
-      resolved_unit = Map.put(unit, "desiredState", "launched")
-
-      #fleet_api requires that name be an atom, so ensure that it's present
-      if (resolved_unit[:name] == nil && resolved_unit["name"] != nil) do
-        orig_unit_name = hd(String.split(resolved_unit["name"], ".service"))
-        unit_instance_name = "#{orig_unit_name}#{instance_id}.service"
-        resolved_unit = Map.put(resolved_unit, :name, unit_instance_name)
-        resolved_unit = Map.put(resolved_unit, "name", unit_instance_name)
-      end
+      resolved_unit = %{unit | desiredState: "launched"}
 
       #check to see if a unit with the same name already is running
-      existing_unit = Enum.reduce(existing_units, nil, fn(cur_unit, existing_unit)->
-        if ((existing_unit == nil) && String.contains?(cur_unit["name"], resolved_unit["name"])) do
-          existing_unit = cur_unit
-        end
-        existing_unit
+      existing_unit = Enum.find(existing_units, fn u ->
+        String.contains?(u.name, resolved_unit.unit)
       end)
 
       #if the same unit name is running, stop it and track the remaining units
@@ -86,45 +80,51 @@ defmodule ProjectOmeletteManager.EtcdCluster.Units do
         port = 0
       end
             
-      if resolved_unit["options"] != nil && length(resolved_unit["options"]) > 0 do
-        new_options= Enum.reduce resolved_unit["options"], [], fn (option, new_options) ->
-          if String.contains?(option["value"], "<%=") do
-            updated_option_value = EEx.eval_string(option["value"], [dst_port: port])
-            updated_option = Map.put(option, "value", updated_option_value)
-          else
-            updated_option = option
-          end
+      if resolved_unit.options != nil && length(resolved_unit.options) > 0 do
+        new_options = resolved_unit.options
+                      |> Enum.map(fn option ->
+                        if String.contains?(option.value, "<%=") do
+                          new_value = EEx.eval_string(option.value, [dst_port: port])
+                          %{option | value: new_value}
+                        else
+                          option
+                        end
+                      end)
 
-          new_options ++ [updated_option]
-        end
-        resolved_unit = Map.put(resolved_unit, "options", new_options)
+        resolved_unit = %{resolved_unit | options: new_options}
       end
-      
+
       #spin up the new unit
-      case SystemdUnit.create(resolved_unit) do
-        {:ok, deployed_unit} ->
-          case SystemdUnit.spinup_unit(deployed_unit, etcd_token) do
+      systemd_unit = %SystemdUnit{
+        etcd_token: etcd_token,
+        dst_port: port,
+        fleet_unit: resolved_unit}
+
+      case SystemdUnit.create(systemd_unit) do
+        {:ok, pid} ->
+          case SystemdUnit.spin_up_unit(pid, etcd_token) do
             true ->
-              SystemdUnit.set_etcd_token(deployed_unit, etcd_token)
-              SystemdUnit.set_assigned_port(deployed_unit, port)
-              newly_deployed_units = newly_deployed_units ++ [deployed_unit]
+              newly_deployed_unit_pids = newly_deployed_unit_pids ++ [pid]
             false ->
-              Logger.error("Unable to monitor instance #{resolved_unit["name"]}")
+              Logger.error("Unable to monitor instance #{resolved_unit.name}")
           end
-        {:error, reason} -> Logger.error("Failed to create systemd unit for #{resolved_unit["name"]}:  #{reason}")
+        {:error, reason} -> Logger.error("Failed to create systemd unit for #{resolved_unit.name}:  #{reason}")
       end
 
       #continue to spin up new units
-      cycle_single_unit(unit, instance_id + 1, max_instances, available_ports, etcd_token, {remaining_units, newly_deployed_units, available_ports})
+      cycle_single_unit(unit, instance_id + 1, max_instances, available_ports, etcd_token, {remaining_units, newly_deployed_unit_pids, available_ports})
     end
   end
 
   defp teardown_units([unit | remaining_units], etcd_token) do
-    case SystemdUnit.create(unit) do
-      {:ok, deployed_unit} -> 
-        SystemdUnit.set_etcd_token(deployed_unit, etcd_token)
-        SystemdUnit.teardown_unit(deployed_unit, etcd_token)
-      {:error, reason} -> Logger.error("Unable to teardown unit #{unit["name"]}:  #{reason}")
+    systemd_unit = %SystemdUnit{
+      etcd_token: etcd_token,
+      fleet_unit: unit
+    }
+    case SystemdUnit.create(systemd_unit) do
+      {:ok, pid} -> 
+        SystemdUnit.tear_down_unit(pid, etcd_token)
+      {:error, reason} -> Logger.error("Unable to teardown unit #{unit.name}:  #{reason}")
     end
     
     teardown_units(remaining_units, etcd_token)
--- test/etcd_cluster/units_test.exs
+++ test/etcd_cluster/units_test.exs
@@ -3,26 +3,118 @@ defmodule ProjectOmeletteManager.EtcdCluster.Units.Test do
 
   import ProjectOmeletteManager.EtcdCluster.Units
 
+  alias ProjectOmeletteManager.SystemdUnit
+
   setup do
-    # :meck.new(FleetApi.Unit)
+    :meck.new(FleetApi.Etcd)
+    :meck.new(SystemdUnit, [:passthrough])
 
     on_exit fn -> :meck.unload end
   end
 
   test "deploy_units -- no units" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, []})
+
     assert [] == deploy_units([], "some_etcd_token")
   end
 
   test "deploy_units --no units and specify ports" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, []})
+
     assert [] == deploy_units([], "some_etcd_token", [1, 2, 3, 4, 5])
   end
 
   test "deploy_units -- unit without .service suffix" do
-    unit = %{"name" => "#{UUID.uuid1()}"}
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, []})
 
-    assert [] = deploy_units([unit], "some_etcd_token")
+    fleet_unit = %FleetApi.Unit{name: "#{UUID.uuid1()}"}
+
+    assert [] = deploy_units([fleet_unit], "some_etcd_token")
   end
 
   test "deploy_units -- units with create failing" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, []})
+    :meck.expect(SystemdUnit, :create, 1, {:error, "bad news bears"})
+
+    fleet_unit_1 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [] = deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token")
   end
+
+  test "deploy_units - units with spinup failing" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, []})
+    :meck.expect(SystemdUnit, :create, 1, {:ok, :some_pid})
+    :meck.expect(SystemdUnit, :spin_up_unit, 2, false)
+
+    fleet_unit_1 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [] = deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token")
+  end  
+
+  test "deploy_units - success" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, [%FleetApi.Machine{}]})
+    :meck.expect(SystemdUnit, :create, 1, {:ok, :some_pid})
+    :meck.expect(SystemdUnit, :spin_up_unit, 2, true)
+
+    fleet_unit_1 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [:some_pid] == deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token")
+  end  
+
+  test "deploy_units - success with provided ports" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, [%FleetApi.Machine{}]})
+    :meck.expect(SystemdUnit, :create, 1, {:ok, :some_pid})
+    :meck.expect(SystemdUnit, :spin_up_unit, 2, true)
+
+    fleet_unit_1 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [:some_pid, :some_pid] == deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token", [12345, 67890])
+  end  
+
+  test "deploy_units - success with template options" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, []})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, [%FleetApi.Machine{}]})
+    :meck.expect(SystemdUnit, :create, 1, {:ok, :some_pid})
+    :meck.expect(SystemdUnit, :spin_up_unit, 2, true)
+
+    fleet_unit_1 = %FleetApi.Unit{
+      name: "#{UUID.uuid1()}.service",
+      options: [%FleetApi.UnitOption{value: "<%= dst_port %>"}]}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [:some_pid, :some_pid] == deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token", [12345, 67890])
+  end    
+
+  test "deploy_units - teardown previous units" do
+    :meck.expect(FleetApi.Etcd, :start_link, 1, {:ok, :some_pid})
+    :meck.expect(FleetApi.Etcd, :list_units, 1, {:ok, [%FleetApi.Unit{name: "test_unit"}]})
+    :meck.expect(FleetApi.Etcd, :list_machines, 1, {:ok, [%FleetApi.Machine{}]})
+    :meck.expect(SystemdUnit, :create, 1, {:ok, :some_pid})
+    :meck.expect(SystemdUnit, :spin_up_unit, 2, true)
+    :meck.expect(SystemdUnit, :tear_down_unit, 2, true)
+
+    fleet_unit_1 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+    fleet_unit_2 = %FleetApi.Unit{name: "#{UUID.uuid1()}.service"}
+
+    assert [:some_pid] == deploy_units([fleet_unit_1, fleet_unit_2], "some_etcd_token")
+  end 
 end
