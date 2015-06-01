13125dd4d278a57c2577a2280dbfda20362aed37
Mike Binns <mike.binns@perceptivesoftware.com>
2015-06-01 10:53:29 -0400

    added index to etcd_clusters:hosting_provider_id

--- priv/repo/migrations/20150528183230_add_cloud_providers_to_etcd.exs
+++ priv/repo/migrations/20150528183230_add_cloud_providers_to_etcd.exs
@@ -7,5 +7,6 @@ defmodule OpenAperture.Manager.Repo.Migrations.AddCloudProvidersToEtcd do
       remove :hosting_provider_region
       add :hosting_provider_id, references(:cloud_providers)
     end
+    create index(:etcd_clusters, [:hosting_provider_id], unique: false)
   end
 end
