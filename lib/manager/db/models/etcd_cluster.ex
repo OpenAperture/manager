a52ce7ca7fe526dd993039a6808ce013713416e1
Mike Binns <mike.binns@perceptivesoftware.com>
2015-06-11 12:19:48 -0400

    added etcd cluster name

--- lib/manager/db/models/etcd_cluster.ex
+++ lib/manager/db/models/etcd_cluster.ex
@@ -2,7 +2,7 @@ require Logger
 
 defmodule OpenAperture.Manager.DB.Models.EtcdCluster do
   @required_fields [:etcd_token]
-  @optional_fields [:hosting_provider_id, :allow_docker_builds, :messaging_exchange_id]
+  @optional_fields [:name, :hosting_provider_id, :allow_docker_builds, :messaging_exchange_id]
   use OpenAperture.Manager.DB.Models.BaseModel
 
   alias OpenAperture.Manager.DB.Models.EtcdClusterPort
@@ -13,6 +13,7 @@ defmodule OpenAperture.Manager.DB.Models.EtcdCluster do
   schema "etcd_clusters" do
     has_many :etcd_cluster_ports,   EtcdClusterPort
     field :etcd_token               # defaults to type :string
+    field :name                     # defaults to type :string
     belongs_to :hosting_provider,   CloudProvider
     field :allow_docker_builds,     :boolean
     belongs_to :messaging_exchange, MessagingExchange
@@ -21,7 +22,7 @@ defmodule OpenAperture.Manager.DB.Models.EtcdCluster do
 
   def validate_changes(model_or_changeset, params) do
     changeset = cast(model_or_changeset, params, @required_fields, @optional_fields)
-    case Map.get(params, :hosting_provider_id, nil) do 
+    changeset = case Map.get(params, :hosting_provider_id, nil) do 
       nil -> 
         changeset
       hosting_provider_id -> 
@@ -29,6 +30,12 @@ defmodule OpenAperture.Manager.DB.Models.EtcdCluster do
             nil -> add_error(changeset, :hosting_provider_id, "Invalid Cloud Provider")
             _provider -> changeset
         end
+    end
+    case Map.get(params, :name, nil) do
+      nil -> changeset
+      _   -> changeset
+             |> validate_length(:name, min: 1)
+             |> validate_length(:name, max: 255)
     end
   end
 
