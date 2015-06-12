a613f0895fae856221d36d9380dc5e5b2302ff29
aandrieiev <andreev00@gmail.com>
2015-06-12 18:19:23 +0200

    Add basic User CRUD

---
+++ test/web/controllers/users_test.exs
@@ -0,0 +1,96 @@
+defmodule OpenAperture.Manager.Controllers.UsersTest do
+  use ExUnit.Case, [async: true]
+  use Phoenix.ConnTest
+
+  alias OpenAperture.Manager
+  alias Manager.DB.Models.User
+  alias Manager.Repo
+  alias Manager.Plugs.Authentication
+
+  @endpoint OpenAperture.Manager.Endpoint
+  @params %{first_name: "John", last_name: "Doe", email: "jdoe@mail.com"}
+
+  setup_all do
+    user = User.new(@params) |> Repo.insert
+
+    :meck.new(Authentication, [:passthrough])
+    :meck.expect(Authentication, :call, fn conn, _opts -> conn end)
+
+    on_exit fn ->
+      :meck.unload
+      Repo.delete_all(User)
+    end
+
+    {:ok, %{user: user}}
+  end
+
+  test "index action", %{user: user} do
+    conn = get(conn, "/users")
+    assert conn.status == 200
+
+    body = Poison.decode!(conn.resp_body)
+    assert length(body) > 0
+
+    user_entry = Enum.fetch!(body, 0)
+    assert user_entry["id"]         == user.id
+    assert user_entry["first_name"] == user.first_name
+    assert user_entry["last_name"]  == user.last_name
+    assert user_entry["email"]      == user.email
+  end
+
+  test "show action - found", %{user: user} do
+    conn = get(conn, "/users/#{user.id}")
+    assert conn.status == 200
+
+    body = Poison.decode!(conn.resp_body)
+    assert body["id"] == user.id
+  end
+
+  test "show action - not found" do
+    assert get(conn, "users/1").status == 404
+  end
+
+  test "create action - success" do
+    params =
+      %{id: 2, first_name: "Josh", last_name: "Done", email: "jdone@mail.com"}
+
+    conn = post(conn, "users", params)
+    assert conn.status == 201
+  end
+
+  test "create action - bad request on invalid values" do
+    conn = post(conn, "users", %{})
+    assert conn.status    == 400
+    assert conn.resp_body =~ ~r/first_name/
+    assert conn.resp_body =~ ~r/last_name/
+    assert conn.resp_body =~ ~r/email/
+  end
+
+  test "update action - success" do
+    user = User.new(%{@params | email: "up@mail.com"}) |> Repo.insert
+    update = %{first_name: "Johanna", last_name: "Dawn", email: "jdawn@mail.com"}
+    conn = put(conn, "users/#{user.id}", update)
+
+    assert conn.status == 204
+  end
+
+  test "update action - no user found" do
+    assert put(conn, "users/1").status == 404
+  end
+
+  test "update action - fails on invalid values", %{user: user} do
+    conn = put(conn, "users/#{user.id}", %{email: "test"})
+    assert conn.status    == 400
+    assert conn.resp_body =~ ~r/email/
+  end
+
+  test "delete action - success" do
+    user = User.new(%{@params | email: "del@mail.com"}) |> Repo.insert
+    conn = delete(conn, "users/#{user.id}")
+    assert conn.status == 204
+  end
+
+  test "delete action - not found" do
+    assert delete(conn, "users/1").status == 404
+  end
+end
---
+++ web/controllers/users.ex
@@ -0,0 +1,87 @@
+defmodule OpenAperture.Manager.Controllers.Users do
+  require Logger
+  use     OpenAperture.Manager.Web, :controller
+  alias   OpenAperture.Manager.DB.Models.User
+
+  plug :action
+
+  @no_user_error "No user with such ID found"
+
+  @doc "GET /users"
+  def index(conn, _params) do
+    users = User
+      |> Repo.all
+      |> Enum.map &Map.from_struct/1
+
+    json conn, users
+  end
+
+  @doc "POST /users"
+  def create(conn, params) do
+    new_user = User.new(params)
+
+    if new_user.valid? do
+      record = Repo.insert(new_user)
+      Logger.info("New user created: #{inspect record}")
+      conn
+      |> put_status(:created)
+      |> json "Created user #{new_user.changes.email}"
+    else
+      conn
+      |> put_status(:bad_request)
+      |> json inspect(new_user.errors)
+    end
+  end
+
+  @doc "GET /users/:id"
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+
+    if user do
+      json conn, user
+    else
+      conn
+      |> put_status(:not_found)
+      |> json @no_user_error
+    end
+  end
+
+  @doc "PUT /users/:id"
+  def update(conn, %{"id" => id} = params) do
+    user = Repo.get(User, id)
+
+    case user do
+      nil ->
+        conn
+        |> put_status(:not_found)
+        |> json @no_user_error
+      user ->
+        changeset = User.validate_changes(user, params)
+
+        if changeset.valid? do
+          record = Repo.update(changeset)
+          Logger.info("User has been updates: #{inspect record}")
+          resp(conn, :no_content, "")
+        else
+          conn
+          |> put_status(:bad_request)
+          |> json inspect(changeset.errors)
+        end
+    end
+  end
+
+  @doc "DELETE /users/:id"
+  def delete(conn, %{"id" => id} = params) do
+    user = Repo.get(User, id)
+
+    if user do
+      Repo.delete(user)
+      Logger.info("User has been deleted: #{inspect user}")
+      resp(conn, :no_content, "")
+    else
+      conn
+      |> put_status(:not_found)
+      |> json @no_user_error
+    end
+  end
+end
