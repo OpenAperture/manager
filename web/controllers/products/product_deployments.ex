86bfcde89e83523eb5cf6caca1230e3a704fa3c5
valevalorin <leucochroic@gmail.com>
2015-07-21 08:57:33 -0500

    scrivener

--- web/controllers/products/product_deployments.ex
+++ web/controllers/products/product_deployments.ex
@@ -16,6 +16,9 @@ defmodule OpenAperture.Manager.Controllers.ProductDeployments do
   alias OpenAperture.Manager.DB.Models.ProductDeploymentPlan
   alias OpenAperture.Manager.DB.Models.ProductDeploymentStep
 
+  alias OpenAperture.ProductDeploymentOrchestratorApi.Request, as: OrchestratorRequest
+  alias OpenAperture.ProductDeploymentOrchestratorApiu.ProductDeploymentOrchestrator.Publisher, as: OrchestratorPublisher
+
   @deployment_sendable_fields [:id, :product_id, :product_deployment_plan_id, :product_environment_id, :execution_options, :completed, :duration, :output, :inserted_at, :updated_at]
   @deployment_steps_sendable_fields [:id, :product_deployment_plan_step_id, :product_deployment_plan_step_type, :duration, :successful, :execution_options, :output, :sequence, :inserted_at, :updated_at]
 
@@ -32,13 +35,20 @@ defmodule OpenAperture.Manager.Controllers.ProductDeployments do
         |> put_status(:not_found)
         |> json ResponseBodyFormatter.error_body(:not_found, "ProductDeployment")
       product ->
-        deployments = product.id
-                      |> DeploymentQuery.get_deployments
-                      |> Repo.all
-                      |> Enum.map(&to_sendable(&1, @deployment_sendable_fields))
+        if params["page"] == nil do 
+          params["page"] = 0
+        end
+
+        page = ProductDeployment
+          |> where([p], p.product_id = ^product_id)
+          |> order_by([p], desc: p.inserted_at)
+          |> Repo.paginate(page: params["page"])
+
+        deployments = page.entries
+          |> Enum.map(&to_sendable(&1, @deployment_sendable_fields))
 
         conn
-        |> json deployments
+        |> json {deployments: deployments, total_pages: page.total_pages, total_deployments: page.total_entries}
     end
   end
 
@@ -83,14 +93,14 @@ defmodule OpenAperture.Manager.Controllers.ProductDeployments do
         if changeset.valid? do
           deployment = Repo.insert(changeset)
 
-          # TODO: Execute deployment plan
-          # DeploymentPlan.execute(%{
-          # deployment: deployment,
-          # deployment_id: deployment.id,
-          # product: product,
-          # plan: plan,
-          # execution_options: params["execution_options"]
-          # })
+          #TODO: Execute deployment plan
+          DeploymentPlan.execute(%{
+          deployment: deployment,
+          deployment_id: deployment.id,
+          product: product,
+          plan: plan,
+          execution_options: params["execution_options"]
+          })
 
           path = product_deployments_path(Endpoint, :show, product_name, deployment.id)
           conn
@@ -154,6 +164,58 @@ defmodule OpenAperture.Manager.Controllers.ProductDeployments do
                 |> Enum.map(&(to_sendable(&1, @deployment_steps_sendable_fields)))
         conn
         |> json steps
+    end
+  end
+
+  @spec execute(term, [any]) :: term
+  def execute(conn, %{"id" => id} = params) do
+    raw_workflow = get_workflow(id)
+
+    cond do
+      raw_workflow == nil -> resp(conn, :not_found, "")
+      raw_workflow.workflow_completed == true -> resp(conn, :conflict, "Workflow has already completed")
+      raw_workflow.current_step != nil -> resp(conn, :conflict, "Workflow has already been started")
+      true ->
+        payload = List.first(convert_raw_workflows([raw_workflow]))
+        if params["force_build"] != nil do
+          payload = Map.put(payload, :force_build, params["force_build"])
+        end
+
+        build_messaging_exchange_id = to_string(params["build_messaging_exchange_id"])
+        if String.length(build_messaging_exchange_id) > 0 do
+          payload = case Integer.parse(build_messaging_exchange_id) do
+            {messaging_exchange_id, _} -> Map.put(payload, :build_messaging_exchange_id, messaging_exchange_id)
+            :error -> payload
+          end
+        end
+
+        deploy_messaging_exchange_id = to_string(params["deploy_messaging_exchange_id"])
+        if String.length(deploy_messaging_exchange_id) > 0 do
+          payload = case Integer.parse(deploy_messaging_exchange_id) do
+            {messaging_exchange_id, _} -> Map.put(payload, :deploy_messaging_exchange_id, messaging_exchange_id)
+            :error -> payload
+          end
+        end        
+                
+        request = OrchestratorRequest.from_payload(payload)
+        request = %{request | notifications_exchange_id: Configuration.get_current_exchange_id}
+        request = %{request | notifications_broker_id: Configuration.get_current_broker_id}
+        request = %{request | workflow_orchestration_exchange_id: Configuration.get_current_exchange_id}
+        request = %{request | workflow_orchestration_broker_id: Configuration.get_current_broker_id}
+        request = %{request | orchestration_queue_name: "workflow_orchestration"}
+
+        case OrchestratorPublisher.execute_orchestration(request) do
+          :ok -> 
+            path = OpenAperture.Manager.Router.Helpers.workflows_path(Endpoint, :show, id)
+
+            # Set location header
+            conn
+            |> put_resp_header("location", path)
+            |> resp(:accepted, "")
+          {:error, reason} -> 
+            Logger.error("Error executing Workflow #{id}: #{inspect reason}")
+            resp(conn, :internal_server_error, "")            
+        end
     end
   end
 
