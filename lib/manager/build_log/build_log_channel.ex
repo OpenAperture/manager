96852884e09b2c5eec3712ace8770cfa26c8f785
Mike Binns <mike.binns@perceptivesoftware.com>
2015-07-30 15:29:02 -0400

    fixed issue with broadcasting

--- lib/manager/build_log/build_log_channel.ex
+++ lib/manager/build_log/build_log_channel.ex
@@ -3,10 +3,9 @@ require Logger
 defmodule OpenAperture.Manager.BuildLogChannel do
   use Phoenix.Channel
 
-  def join("build_log:" <> workflow_id, auth_msg, socket) do
-    Logger.debug("[BuildLogChannel] join for workflow: #{workflow_id}, auth_msg: #{inspect auth_msg}")
+  def join("build_log:" <> workflow_id, _auth_msg, socket) do
+    Logger.debug("[BuildLogChannel] join for workflow: #{workflow_id}")
     {:ok, socket}
   end
-
 
 end
